b7015c64d56e3bfe43847e1fb4af54af83dae292
Nohan Budry <nohan.budry@heig-vd.ch>
2021-04-28 13:28:22 +0200

    setup pun

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public override void OnPlayerEnteredRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+
+            LoadArena();
+        }
+    }
+
+    public override void OnPlayerLeftRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+
+            LoadArena();
+        }
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    private void LoadArena()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+        }
+        Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+        PhotonNetwork.LoadLevel("Room " + PhotonNetwork.CurrentRoom.PlayerCount);
+    }
+}
---
+++ Assets/Scripts/Laucher.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Laucher : MonoBehaviourPunCallbacks
+{
+    private string gameVersion = "1";
+
+    [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+    [SerializeField]
+    private byte maxPlayersPerRoom = 3;
+
+    [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+    [SerializeField]
+    private GameObject controlPanel;
+
+    [Tooltip("The UI Label to inform the user that the connection is in progress")]
+    [SerializeField]
+    private GameObject progressLabel;
+
+    private bool isConnecting;
+
+    private void Awake()
+    {
+        Debug.Log("Awake");
+
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    private void Start()
+    {
+        controlPanel.SetActive(true);
+        progressLabel.SetActive(false);
+    }
+
+    public void Connect()
+    {
+        controlPanel.SetActive(false);
+        progressLabel.SetActive(true);
+
+        Debug.Log("Connect");
+
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            isConnecting = PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+
+    #region MonoBehaviourPunCallbacks Callbacks
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+
+        if (isConnecting)
+        {
+            PhotonNetwork.JoinRandomRoom();
+            isConnecting = false;
+        }
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        controlPanel.SetActive(true);
+        progressLabel.SetActive(false);
+
+        Debug.LogWarningFormat("Disconnect with reason {0}", cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Join random room failed. Creating new room...");
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined room");
+
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            Debug.Log("Load Room 1");
+
+            PhotonNetwork.LoadLevel("Room 1");
+        }
+    }
+
+    #endregion
+}
